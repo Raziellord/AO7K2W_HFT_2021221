8cdb2335e40d58a2d035b8bf1746cb4c3bda92f5
Raziellord <fischer.vanya@gmail.com>
2021-11-19 19:17:53 +0100

    CrewLogic created

---
+++ AO7K2W_HFT_2021221.Logic/CrewLogic.cs
@@ -0,0 +1,43 @@
+﻿using AO7K2W_HFT_2021221.Models;
+using AO7K2W_HFT_2021221.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AO7K2W_HFT_2021221.Logic
+{
+    public class CrewLogic : ICrewLogic
+    {
+        ICrewRepository crewRepo;
+        public CrewLogic(ICrewRepository crewRepo)
+        {
+            this.crewRepo = crewRepo;
+        }
+        public void Create(Crew crew)
+        {
+            crewRepo.Create(crew);
+        }
+
+        public void Delete(int id)
+        {
+            crewRepo.Delete(id);
+        }
+
+        public Crew Read(int id)
+        {
+            return crewRepo.Read(id);
+        }
+
+        public IEnumerable<Crew> ReadAll()
+        {
+            return crewRepo.ReadAll();
+        }
+
+        public void Update(Crew crew)
+        {
+            crewRepo.Update(crew);
+        }
+    }
+}
