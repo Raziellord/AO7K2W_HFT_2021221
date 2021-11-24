47bfc25c29df30afd62077296241340035c1f3b0
Raziellord <fischer.vanya@gmail.com>
2021-11-24 18:59:14 +0100

    Client rest service created, successful debug start after endpoint started.

--- AO7K2W_HFT_2021221.Client/Program.cs
+++ AO7K2W_HFT_2021221.Client/Program.cs
@@ -1,4 +1,5 @@
 ﻿using AO7K2W_HFT_2021221.Data;
+using AO7K2W_HFT_2021221.Models;
 using System;
 using System.Linq;
 
@@ -8,37 +9,17 @@ namespace AO7K2W_HFT_2021221.Client
     {
         static void Main(string[] args)
         {
-            var ctx = new TankDbContext();
-
-            Console.WriteLine("----Conflicts----");
-            foreach (var conflict in ctx.Conflicts)
+            RestService rest = new RestService("http://localhost:26569");
+            rest.Post<Conflict>(new Conflict()
             {
-                Console.WriteLine(conflict.NameOfConflict);
-            }
-            Console.WriteLine("----Tanks----");
-            foreach (var tank in ctx.Tanks)
+                NameOfConflict = "Operation Teszt Conflict",
+                DateOfConflict = DateTime.Parse("2000.01.01")
+            }, "conflict");
+            var conflicts = rest.Get<Conflict>("conflict");
+            foreach (var item in conflicts)
             {
-                Console.WriteLine(tank.Nickname);
+                Console.WriteLine(item.NameOfConflict);
             }
-            Console.WriteLine("----Crews----");
-            foreach (var crew in ctx.Crews)
-            {
-                Console.WriteLine(crew.Name);
-            }
-            Console.WriteLine("-------------");
-            foreach (var tanks in ctx.Tanks)
-            {
-                Console.WriteLine("Tank name: " + tanks.Nickname);
-            }
-
-            
-            foreach (var tanks in ctx.Tanks)
-            {
-                
-            }
-            Console.ReadLine();
-
-
             
         }
     }
